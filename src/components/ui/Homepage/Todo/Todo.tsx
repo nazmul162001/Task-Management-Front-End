5027ac7ec7aa5c7ff18f2f19859aea483bcac7a7
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-12-11 15:29:15 +0600

    initialize next project for taskmanagement application

---
+++ src/components/ui/Homepage/Todo/Todo.tsx
@@ -0,0 +1,11 @@
+const Todo = () => {
+  return (
+    <div className="text-white">
+      <h1 className="px-16 py-5 bg-[#421C98] rounded text-3xl">
+        Welcome to my Todo Application
+      </h1>
+    </div>
+  );
+};
+
+export default Todo;
