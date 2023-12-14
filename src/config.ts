1c97c66465dcbe8ca8c8a346ea2177dbccc024bf
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-12-14 14:59:41 +0600

    update url & ready to deploy

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // Local Link 
-export const BASE_URL = 'http://localhost:5000'
+// export const BASE_URL = 'http://localhost:5000'
 // LIVE Link 
-// export const BASE_URL = ''
+export const BASE_URL = 'task-management-backend-coral.vercel.app'
